f5715ff9c674b670bcd74fe6986f9fa8ec5264af
Ford Seidel <flseidel@gmail.com>
2015-02-25 10:15:25 -0500

    Revert "Delete buildUI.sh"
    This reverts commit 77d724330c3377eeaef1c0c74f37eafafbb761cc.

---
+++ buildUI.sh
@@ -0,0 +1,1 @@
+clang /usr/local/Cellar/portaudio/19.20140130/lib/libportaudio.dylib /usr/local/Cellar/libsamplerate/0.1.8/lib/libsamplerate.dylib -lncurses -std=c99 -pedantic -Wall -Werror -Wextra -Wuninitialized MFoPUI.c -o MFoPUI -O3
