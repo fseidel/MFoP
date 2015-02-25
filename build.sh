e854f71d17a0a03709fddb0b230662a168fdefed
Ford Seidel <flseidel@gmail.com>
2015-02-25 10:16:29 -0500

    This reverts commit 77d724330c3377eeaef1c0c74f37eafafbb761cc.

--- build.sh
+++
@@ -1,1 +0,0 @@
-clang /usr/local/Cellar/portaudio/19.20140130/lib/libportaudio.dylib /usr/local/Cellar/libsamplerate/0.1.8/lib/libsamplerate.dylib -std=c99 -pedantic -Wall -Werror -Wextra -Wuninitialized MFoP.c -o MFoP -O3
